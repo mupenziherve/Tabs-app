26d6427d6602220df71873ebf719cb2ff8f450c5
Mupenzi Herve <mupenziherve04@gmail.com>
2025-07-30 23:22:12 +0200

    tab app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  return <h2>Tabs Starter Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam eveniet molestiae eos, vitae dolore voluptates ipsum dignissimos temporibus vel nam quas consequatur error nostrum excepturi totam nulla quae rerum odit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae consectetur veniam temporibus illo. Ea natus sint, ex voluptatibus ducimus dolores a quaerat fuga voluptas velit architecto, totam aspernatur accusamus quia!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, suscipit repellendus, quos ea ipsam sit debitis doloribus dolor, ullam animi dolore doloremque maiores repellat a qui porro aperiam. Soluta, minima.
+  </h2>;
 };
 export default App;
